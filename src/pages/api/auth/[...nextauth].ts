2f5e139bf0a1bb6cf171b4d512fc6ac1bc47d7ee
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2021-06-06 19:50:21 -0300

    ✨ Add faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,27 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
+import { query as q } from 'faunadb'
+import { fauna } from '../../../services/fauna'
+
 export default NextAuth({
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      scope: process.env.GITHUB_SCOPE,
+      scope: 'read:user',
     }),
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+      const { email } = user
+      await fauna.query(
+        q.Create(
+          q.Collection('users'),
+          { data: { email: email } }
+        )
+      )
+      return true
+    }
+  }
 })
